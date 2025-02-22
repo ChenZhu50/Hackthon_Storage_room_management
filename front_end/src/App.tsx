e4b661753590071d95e573ed9ac072ed382ddc74
Brian Niski <50961607+niskb@users.noreply.github.com>
2025-02-22 15:46:13 -0500

    Add CSS

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SignUp from './components/signup';
 import Login from './components/login';
+import './index.css';
 
 const App: React.FC = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center',  gap: '20px', padding: '20px'}}>
-      <Login />
-      <SignUp />
+    <div>
+      <h2>Welcome to the app!</h2>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center',  gap: '20px', padding: '20px'}}>
+        <Login />
+        <SignUp />
+      </div>
     </div>
   );
 };
